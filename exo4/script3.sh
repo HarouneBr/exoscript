32622f7d7463f6b5e7b12d0530753bb33e99fa92
HarouneBr <harounemailpro@gmail.com>
2023-11-02 12:28:08 +0100

    exercice 3 et 4

---
+++ exo4/script3.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+public/{css/{footer,header,main},img/{},js}
--- exo4/script4.sh
+++ exo4/script4.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-public/{css/{footer,header,main},img/{},js}
+mkdir public/{css/{footer,header,main},img,js}
+touch public/css/footer/style.css
+touch public/css/header/style.css
+touch public/css/main/style.css
+touch public/img/img1,img2
+touch public/index.html
+touch public/js/main.js
